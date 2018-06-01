bf3a9f8e4455531d922c4489099d384b9882ae5a
Administrator <Administrator@PC-20180515KRAG>
2018-06-01 09:11:19 +0800

    update pull

--- src/main/java/com/anbang/qipai/game/plan/dao/mongodb/repository/NoticeRepository.java
+++ src/main/java/com/anbang/qipai/game/plan/dao/mongodb/repository/NoticeRepository.java
@@ -4,13 +4,14 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.anbang.qipai.game.plan.bean.Notices;
 
+public interface NoticeRepository extends MongoRepository<Notices, String> {
+	/**
+	 * 查询启用状态的公告
+	 * 
+	 * @param state
+	 *            状态
+	 **/
+	public Notices queryByState(Integer state);
 
-public interface NoticeRepository extends MongoRepository<Notices, String>{
-	/**查询启用状态的公告
-	 * @param state 状态
-	
-		public Notices queryByState(Integer state);
-	
-		public Notices queryById(String id);
-	 * **/
+	public Notices queryById(String id);
 }
