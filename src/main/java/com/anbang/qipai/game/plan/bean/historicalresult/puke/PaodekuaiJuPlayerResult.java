80a1219f1253d9b4f8de8152258ceead324488bf
ethan0353 <yinziyu7@gmail.com>
2019-04-09 11:34:40 +0800

    add result

--- src/main/java/com/anbang/qipai/game/plan/bean/historicalresult/puke/PaodekuaiJuPlayerResult.java
+++ src/main/java/com/anbang/qipai/game/plan/bean/historicalresult/puke/PaodekuaiJuPlayerResult.java
@@ -8,20 +8,20 @@ public class PaodekuaiJuPlayerResult implements GameJuPlayerResult {
 	private String playerId;
 	private String nickname;
 	private String headimgurl;
-//	private int shuangkouCount;
-//	private int dankouCount;
-//	private int pingkouCount;
-//	private int maxXianshu;
+	private int danguanCount;
+	private int shuangguanCount;
+	private int boomCount;
+	private int maxScore;
 	private int totalScore;
 
 	public PaodekuaiJuPlayerResult(Map juPlayerResult) {
 		this.playerId = (String) juPlayerResult.get("playerId");
 		this.nickname = (String) juPlayerResult.get("nickname");
 		this.headimgurl = (String) juPlayerResult.get("headimgurl");
-//		this.shuangkouCount = ((Double) juPlayerResult.get("shuangkouCount")).intValue();
-//		this.dankouCount = ((Double) juPlayerResult.get("dankouCount")).intValue();
-//		this.pingkouCount = ((Double) juPlayerResult.get("pingkouCount")).intValue();
-//		this.maxXianshu = ((Double) juPlayerResult.get("maxXianshu")).intValue();
+		this.danguanCount = ((Double) juPlayerResult.get("danguanCount")).intValue();
+		this.shuangguanCount = ((Double) juPlayerResult.get("shuangguanCount")).intValue();
+		this.boomCount = ((Double) juPlayerResult.get("boomCount")).intValue();
+		this.maxScore = ((Double) juPlayerResult.get("maxScore")).intValue();
 		this.totalScore = ((Double) juPlayerResult.get("totalScore")).intValue();
 	}
 
@@ -61,4 +61,35 @@ public class PaodekuaiJuPlayerResult implements GameJuPlayerResult {
 		this.totalScore = totalScore;
 	}
 
+	public int getDanguanCount() {
+		return danguanCount;
+	}
+
+	public void setDanguanCount(int danguanCount) {
+		this.danguanCount = danguanCount;
+	}
+
+	public int getShuangguanCount() {
+		return shuangguanCount;
+	}
+
+	public void setShuangguanCount(int shuangguanCount) {
+		this.shuangguanCount = shuangguanCount;
+	}
+
+	public int getBoomCount() {
+		return boomCount;
+	}
+
+	public void setBoomCount(int boomCount) {
+		this.boomCount = boomCount;
+	}
+
+	public int getMaxScore() {
+		return maxScore;
+	}
+
+	public void setMaxScore(int maxScore) {
+		this.maxScore = maxScore;
+	}
 }
